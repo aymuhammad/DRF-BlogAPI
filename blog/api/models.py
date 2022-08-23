2b28befa35946720546069290a8d7c3b73265d8e
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-24 00:30:27 +0100

    add class posts

--- blog/api/models.py
+++ blog/api/models.py
@@ -6,7 +6,7 @@ from django.db import models
 # model that inherits from Django’s Model class and define its fields
 class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length==100, blank=True, default="")
+    title = models.CharField(max_length=100, blank=True, default="")
     body = models.TextField(blank=True, default="")
     owner = models.ForeignKey('auth.User', related_name='posts', on_delete=models.CASCADE)
 
