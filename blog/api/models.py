995b8a0883e83b80e863327b2540ab17354d398e
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-24 00:29:58 +0100

    add class post

--- blog/api/models.py
+++ blog/api/models.py
@@ -1,3 +1,14 @@
+from statistics import mode
+from turtle import title
+from venv import create
 from django.db import models
 
-# Create your models here.
+# model that inherits from Django’s Model class and define its fields
+class Post(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length==100, blank=True, default="")
+    body = models.TextField(blank=True, default="")
+    owner = models.ForeignKey('auth.User', related_name='posts', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['created']
