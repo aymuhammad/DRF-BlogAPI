b56710370c972bcca2f3e5595af06e3529cc4561
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-24 00:43:06 +0100

    add comment

--- blog/api/views.py
+++ blog/api/views.py
@@ -3,10 +3,12 @@ from rest_framework import generics
 from api import serializers
 from django.contrib.auth.models import User
 
+# provides read-only access (via get) to the list of users
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
 
+# provides read-only access (via get) to a single user
 class UserDetails(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
