fcf89eb996818466e4e2c3ee21e316baf648fe04
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-24 01:18:55 +0100

    add serializers post and user

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,10 +1,28 @@
+from csv import field_size_limit
 from dataclasses import fields
 from statistics import mode
+import turtle
 from rest_framework import serializers
 # users are created from the User model defined in django.contrib.auth
 from django.contrib.auth.models import User
+from . models import Post
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source = 'owner.username')
+
+    class Meta:
+        models = Post
+        fields = ['id', 'title', 'body', 'owner']
+
+class UserSerializer(serializers.ModelSerializer):
+    posts = serializers.PrimaryKeyRelatedField(many=True, read_only = True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'posts']
