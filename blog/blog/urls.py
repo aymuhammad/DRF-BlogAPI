a510c918cac467f067b5701811cd7af91456f9b9
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-24 01:45:35 +0100

    add auth link

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
+    # a Log in button to the browsable API 
+    path('api-auth/', include('rest_framework.urls')),
 ]
