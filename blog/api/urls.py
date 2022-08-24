fdc26dd655eff74810b819b211deedd6a10a32fe
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-24 01:20:40 +0100

    modify with post urls

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -5,6 +5,8 @@ from api import views
 urlpatterns = [
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetails.as_view()),
+    path('posts/', views.PostList.as_view()),
+    path('posts/<int:pk>/', views.PostDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
