2f85b55919cb7c83bb422b3a91e373716bfcef5e
Auwal Yahaya <auwalyah@yahoo.com>
2022-08-24 11:39:42 +0100

    add registeration path and details path

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,6 +1,10 @@
+from atexit import register
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
+
+# registrations link
+from .views import UserDetailAPI, RegisterUserAPIView
 
 urlpatterns = [
     path('users/', views.UserList.as_view()),
@@ -9,6 +13,8 @@ urlpatterns = [
     path('posts/<int:pk>/', views.PostDetail.as_view()),
     path('comments/', views.CommentList.as_view()),
     path('comments/<int:pk>/', views.CommentDetail.as_view()),
+    path('get-details/', UserDetailAPI.as_view()),
+    path('register/', RegisterUserAPIView.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
